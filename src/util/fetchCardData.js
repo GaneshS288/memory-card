987d82fc3d6f1ae953c7d924c9470f72b951bc7d
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-01-27 15:35:06 +0530

    create function to fetch pokemon data

---
+++ src/util/fetchCardData.js
@@ -0,0 +1,48 @@
+const pokeApiUrl = "https://pokeapi.co/api/v2/";
+const apiEndpoint = "pokemon";
+const pokemonNameList = [
+  "pikachu",
+  "bulbasaur",
+  "charizard",
+  "pidgeot",
+  "oshawott",
+  "gible",
+  "greninja",
+  "infernape",
+  "onix",
+  "snivy",
+];
+
+async function fetchPokemonData(pokemonName) {
+  try {
+    const fetchResponse = await fetch(
+      `${pokeApiUrl}/${apiEndpoint}/${pokemonName}`
+    );
+    if (!fetchResponse.ok) {
+      throw new Error(`Response not ok! Status : ${fetchResponse.status}`);
+    }
+    const responseJson = await fetchResponse.json();
+    const pokemonData = {
+      name: `${responseJson.name
+        .charAt(0)
+        .toUpperCase()}${responseJson.name.slice(1)}`,
+      spriteUrl: responseJson.sprites.front_default,
+      id: responseJson.id,
+    };
+
+    return pokemonData;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export default function getPokemonsData(pokemonNameList) {
+  const pokemonDataList = [];
+
+  for (let i = 0; i < pokemonNameList.length; i++) {
+    const pokemonData = fetchPokemonData(pokemonNameList[i]);
+    pokemonData.then((value) => pokemonDataList.push(value));
+  }
+
+  return pokemonDataList;
+}
