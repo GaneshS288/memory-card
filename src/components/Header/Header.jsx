76eefccf1181098b05eb38c4cc3125efe4a64978
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-01-30 21:04:29 +0530

    style header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
+import "./Header.css"
+
 export default function Header({score, highScore}) {
     return(
         <header>
-            <h1 className="main-title">Memory Cards</h1>
+            <h1 className="header-title">Memory Cards</h1>
             <div className="scores-container" >
-                <p className="score">{score}</p>
-                <p className="highscore">{highScore}</p>
+                <p className="score"><span>Score</span> <span>-</span> <span>{score}</span></p>
+                <p className="highscore"><span>HighScore</span> <span>-</span> <span>{highScore}</span></p>
             </div>
         </header>
     )
